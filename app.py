0eec207605efa50c37dd042d45e806fb8f80e0e7
Bankweruth12 <ruthbankwe@icloud.com>
2026-04-30 00:16:33 +0200

    Dernier essai port 10000

--- app.py
+++ app.py
@@ -166,4 +166,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=False, use_reloader=False)
+    app.run(host='0.0.0.0', port=10000, debug=False, use_reloader=False)
